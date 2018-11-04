89b3739fb2e49b45eae60ee16121549e214ee0c2
Ansh Gujral <anshgujral1997@gmail.com>
2018-11-05 00:57:00 +0530

    bodyparser middleware setup

--- server/index.js
+++ server/index.js
@@ -1,5 +1,23 @@
 import express from 'express'
+import path from 'path'
+import cookiesParser from 'cookie-parser'
+import bodyParser from 'body-parser'
+import exphbs from 'express-handlebars'
+import expressValidotor from 'express-validator'
+import flash from 'connect-flash'
+import session from 'express-session'
+import passport from 'passport'
+import localStratergy from 'passport-local'
+import mongo from 'mongodb'
+import mongoose from 'mongoose'
 
+
+// Init App
 const app = express()
+
+// BodyParser Middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookiesParser())
 
 app.listen(3000, ()=>console.log('App is listening at 3000'))
