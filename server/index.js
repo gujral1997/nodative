055cbbaabfde04f3ca8576c8036b49d5368d3858
Ansh Gujral <anshgujral1997@gmail.com>
2018-11-05 01:42:32 +0530

    created View Engine, BodyParser Middleware, Set Static Folder, Middleware Express Session and Express Validator

--- server/index.js
+++ server/index.js
@@ -10,14 +10,47 @@ import passport from 'passport'
 import localStratergy from 'passport-local'
 import mongo from 'mongodb'
 import mongoose from 'mongoose'
+import { param } from 'express-validator/check';
 
 
 // Init App
 const app = express()
 
+// View Engine
+app.set('views', path.join(__dirname, 'views'))
+app.engine('handlebars', exphbs({defaultLayout: 'layout'}))
+app.set('view engine', 'handlebars')
+
 // BodyParser Middleware
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookiesParser())
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Middleware Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}))
+
+// Express Validator (from express validator github)
+app.use(expressValidotor({
+    errorFormatter: (param, msg, value)=> {
+        let namespace = param.split('.'),
+        root = namespace.shift(),
+        formParam = root
+        while(namespace.length) {
+            formParam += '[' +namespace.shift() + ']'
+        }
+        return {
+            param: formParam,
+            msg: msg,
+            value: value
+        }
+    }
+}))
 
 app.listen(3000, ()=>console.log('App is listening at 3000'))
