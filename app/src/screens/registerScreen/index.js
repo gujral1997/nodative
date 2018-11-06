9b7a47b9855b98ce8f99b8e68ef032e565d0bbae
Ansh Gujral <anshgujral1997@gmail.com>
2018-11-06 05:49:28 +0530

    created register screen ui

--- app/src/screens/registerScreen/index.js
+++ app/src/screens/registerScreen/index.js
@@ -1,12 +1,78 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import {
+    View,
+    TextInput,
+    TouchableOpacity,
+    Text,
+    ActivityIndicator,
+    ImageBackground
+} from 'react-native'
+import KeyboardSpacer from 'react-native-keyboard-spacer';
+import {Actions, ActionConst} from  'react-native-router-flux'
+
+import assets from '../../themes/assets'
+import styles from './styles'
+
 
 class RegisterScreen extends React.Component {
+
+    state = {
+
+    }
+
     render() {
         return(
-            <View>
-                <Text>Register Screen</Text>
-            </View>
+            <ImageBackground style={styles.container} source={assets['background']}>
+                <View style={styles.subView}>
+                    <TextInput
+                        style={styles.input}
+                        onChangeText={(name) => this.setState({name})}
+                        value={this.state.name}
+                        placeholder='Name'
+                        placeholderTextColor='black'
+                        />
+                    <TextInput
+                        style={styles.input}
+                        onChangeText={(email) => this.setState({email})}
+                        value={this.state.email}
+                        placeholder='Email'
+                        placeholderTextColor='black'
+                        autoCapitalize = 'none'
+                        />
+                    <TextInput
+                        style={styles.input}
+                        onChangeText={(username) => this.setState({username})}
+                        value={this.state.username}
+                        placeholder='Username'
+                        placeholderTextColor='black'
+                        />
+                    <TextInput
+                        style={styles.input}
+                        onChangeText={(password) => this.setState({password})}
+                        value={this.state.password}
+                        placeholder='Password'
+                        placeholderTextColor='black'
+                        secureTextEntry
+                        />
+                    <TextInput
+                        style={styles.input}
+                        onChangeText={(password1) => this.setState({password1})}
+                        value={this.state.password1}
+                        placeholder='Retype Password'
+                        placeholderTextColor='black'
+                        secureTextEntry
+                        />
+                    <TouchableOpacity 
+                        onPress={()=>{
+                            alert('Submit Pressed')
+                        }}
+                        style={styles.button}
+                        >
+                        {this.state.loading?<ActivityIndicator color="white"/>:<Text style={styles.text}>Submit</Text>}
+                    </TouchableOpacity>
+                    <KeyboardSpacer/>
+                </View>
+            </ImageBackground>
         )
     }
 }
