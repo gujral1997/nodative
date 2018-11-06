941cf438537f04befe497138fb39d01d195d8fed
Ansh Gujral <anshgujral1997@gmail.com>
2018-11-06 05:35:37 +0530

    redux setup

--- app/App.js
+++ app/App.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import {Provider} from 'react-redux'
+import ReduxThunk from 'redux-thunk'
+import {createStore, applyMiddleware} from 'redux'
 
+import reducers from './src/reducers'
 import Routes from './src/Routes'
 
 export default class App extends React.Component {
   render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk))
     return (
-      <Routes/>
-    );
+      <Provider store={store}>
+        <Routes/>
+      </Provider>
+    )
   }
 }
 
